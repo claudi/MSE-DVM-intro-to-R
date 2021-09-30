53a7f04f433b590c5e5854e56e30ecd706fd0143
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-09-30 10:09:23 +0200

    Load dataframe
    From `https://developer.tmb.cat/data/parades`

--- ex3/claudilleydamolto-3.R
+++ ex3/claudilleydamolto-3.R
@@ -5,3 +5,11 @@
 #ls()
 
 # ----------
+parades <- read.csv("parades.csv", header = TRUE, sep = ",", quote = "\"")
+
+parades$NOM_TIPUS_PARADA <- as.factor(parades$NOM_TIPUS_PARADA)
+parades$TIPIFICACIO_PARADA <- as.factor(parades$TIPIFICACIO_PARADA)
+parades$ID_POBLACIO <- as.factor(parades$ID_POBLACIO)
+parades$ID_DISTRICTE <- as.factor(parades$ID_DISTRICTE)
+
+pairs(parades[,c("ID_PARADA", "CODI_PARADA", "CODI_INTERC", "LON", "LAT")])
